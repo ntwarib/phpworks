7f022a3fb10192bcc39cede7ec25975bd8cac0b8
ntwarib <82451662+ntwarib@users.noreply.github.com>
2022-02-18 10:45:25 +0200

    18th february 2022
    creating class and give it properties

---
+++ learn 1.php
@@ -0,0 +1,14 @@
+<?php 
+//creating class// note that class must always start with capital letter so that it may be accessed 
+//note that 
+
+class Home{
+    //code
+    public $primaryColor = 'black';
+    public $secondaryColors = {
+        'bathroom' = 'white',
+        'bedroom' = 'Light pink' ,
+        'kicthen' = 'light blue', 
+    };
+}
+?>
